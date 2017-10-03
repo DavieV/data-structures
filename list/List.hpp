8c9263db3f8cda4f6b005049ca5c9e375bddb64f
Quinn Perfetto <thequinnftw@gmail.com>
2017-10-02 18:18:58 -0700

    improved list code

--- list/List.hpp
+++ list/List.hpp
@@ -1,41 +1,40 @@
 #include <iostream>
+#include <memory>
+
 #include "Node.hpp"
 
 template <class T>
 class List {
-    private:
-        Node<T>* head_;
-    public:
-        List() { head_ = nullptr; }
-        List(T data);
-        void add(T data);
-        void print_list();
+ public:
+  List(const T& data) : head_(std::make_unique<Node<T>>(data)) {}
+  List() = default;
+
+  void add(const T& data);
+  void print_list();
+
+ private:
+  std::unique_ptr<Node<T>> head_;
 };
 
 template <class T>
-List<T>::List(T data) {
-    head_ = new Node<T>(data);
-}
-
-template <class T>
-void List<T>::add(T data) {
-    Node<T>* new_node = new Node<T>(data);
-    if (head_ == nullptr) {
-        head_ = new_node;
-    } else {
-        Node<T> *tmp = head_;
-        while (tmp->next_ != nullptr) {
-            tmp = tmp->next_;
-        }
-        tmp->next_ = new_node;
+void List<T>::add(const T& data) {
+  auto new_node = std::make_unique<Node<T>>(data);
+  if (!head_) {
+    head_ = std::move(new_node);
+  } else {
+    Node<T>* tmp = head_.get();
+    while (tmp->next_) {
+      tmp = tmp->next_.get();
     }
+    tmp->next_ = std::move(new_node);
+  }
 }
 
 template <class T>
 void List<T>::print_list() {
-    Node<T> *tmp = head_;
-    while (tmp != nullptr) {
-        std::cout << tmp->data_ << std::endl;
-        tmp = tmp->next_;
-    }
+  Node<T>* tmp = head_.get();
+  while (tmp) {
+    std::cout << tmp->data_ << std::endl;
+    tmp = tmp->next_.get();
+  }
 }
